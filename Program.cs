332507fa04c6a0ad38edb823c88d58de4bb3fab6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-17 22:27:44 -0300

    Piada ruim

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿﻿Console.WriteLine("O que é, o que é... ");
+Console.WriteLine("... que uma impressora disse para a outra?\n");
+
+Thread.Sleep(5000);
+
+Console.WriteLine("Essa folha é tua ou é impressão minha?");
